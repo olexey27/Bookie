e5a8627c08cf6535e90f899a46d1ca57673adeca
Alexey Krasnokutskiy <allxyog@gmail.com>
2023-07-12 00:13:03 +0200

    add Category List

--- Udemy/Controllers/CategoryController.cs
+++ Udemy/Controllers/CategoryController.cs
@@ -1,11 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Udemy.Data;
+using Udemy.Models;
 
 namespace Udemy.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
+            List<Category> objCategoryList = _db.Categories.ToList();
             return View();
         }
     }
