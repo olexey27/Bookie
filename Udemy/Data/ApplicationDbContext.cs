5aef4cd68d8d76a8f232ed2c2431db5f6eefd869
Alexey Krasnokutskiy <allxyog@gmail.com>
2023-07-11 23:13:08 +0200

    add

---
+++ Udemy/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Udemy.Models;
+
+namespace Udemy.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
