5ca0f63961761ad6a048d97aa2f0a3144e8d872b
Alexey Krasnokutskiy <allxyog@gmail.com>
2023-07-18 19:28:16 +0200

    Input Tag Helpers

--- Udemy/Models/Category.cs
+++ Udemy/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Udemy.Models
 {
@@ -7,7 +8,9 @@ namespace Udemy.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [DisplayName("Category Name")]
         public string Name { get; set; }
+        [DisplayName("Display Order")]
         public int DisplayOrder { get; set; }
     }
 }
