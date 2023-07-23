8128d9cfc7257f889c23c7274913cc8456f2c1d3
Alexey Krasnokutskiy <allxyog@gmail.com>
2023-07-24 01:06:20 +0200

    Update Category in Action

--- Udemy/Controllers/CategoryController.cs
+++ Udemy/Controllers/CategoryController.cs
@@ -37,5 +37,31 @@ namespace Udemy.Controllers
             }
             return View();
         }
+        public IActionResult Edit(int? id)
+        {
+            if(id==null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+            //Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u=>u.Id==id);
+            //Category? categoryFromDb2 = _db.Categories.Where(u=>u.Id==id).FirstOrDefault();
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
     }
 }
