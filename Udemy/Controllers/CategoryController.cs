82688d793e8a36922e051f2e61b21167c7efe038
Alexey Krasnokutskiy <allxyog@gmail.com>
2023-07-23 23:23:22 +0200

    Server Side Validations

--- Udemy/Controllers/CategoryController.cs
+++ Udemy/Controllers/CategoryController.cs
@@ -25,9 +25,13 @@ namespace Udemy.Controllers
         [HttpPost]
         public IActionResult Create(Category obj)
         {
-            _db.Categories.Add(obj);
-            _db.SaveChanges();
-            return RedirectToAction("Index");
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
         }
     }
 }
