bf7d160769629995a77d5b1a9aedc6d2e4cd35a3
Alexey Krasnokutskiy <allxyog@gmail.com>
2023-07-23 23:34:30 +0200

    Custom Validations

--- Udemy/Controllers/CategoryController.cs
+++ Udemy/Controllers/CategoryController.cs
@@ -25,6 +25,10 @@ namespace Udemy.Controllers
         [HttpPost]
         public IActionResult Create(Category obj)
         {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            }
             if (ModelState.IsValid)
             {
                 _db.Categories.Add(obj);
