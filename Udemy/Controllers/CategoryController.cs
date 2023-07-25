7e373550b4aaa051e67740f214deeabf298b1d8a
Alexey Krasnokutskiy <allxyog@gmail.com>
2023-07-26 01:58:26 +0200

    änderung

--- Udemy/Controllers/CategoryController.cs
+++ Udemy/Controllers/CategoryController.cs
@@ -33,6 +33,7 @@ namespace Udemy.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
                 return RedirectToAction("Index");
             }
             return View();
@@ -59,6 +60,7 @@ namespace Udemy.Controllers
             {
                 _db.Categories.Update(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
                 return RedirectToAction("Index");
             }
             return View();
@@ -88,6 +90,7 @@ namespace Udemy.Controllers
             }
             _db.Categories.Remove(obj);
             _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
             return RedirectToAction("Index");
         }
     }
