b3502227ed04787cbfd6d7ba810c75fd23c7c9e2
Alexey Krasnokutskiy <allxyog@gmail.com>
2023-07-28 20:34:59 +0200

    View Models in Action

--- Udemy.Models/Product.cs
+++ Udemy.Models/Product.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 
 namespace UdemyBook.Models
 {
@@ -43,7 +44,9 @@ namespace UdemyBook.Models
 
         public int CategoryId { get; set; }
         [ForeignKey("CategoryId")]
+        [ValidateNever]
         public Category Category { get; set; }
+        [ValidateNever]
         public string ImageUrl { get; set; }
     }
 }
--- Udemy/Areas/Admin/Controllers/ProductController.cs
+++ Udemy/Areas/Admin/Controllers/ProductController.cs
@@ -4,6 +4,7 @@ using System.Collections;
 using UdemyBook.DataAccess.Repository.IRepository;
 using UdemyBook.DataAcess.Data;
 using UdemyBook.Models;
+using UdemyBook.Models.ViewModels;
 
 namespace UdemyBook.Areas.Admin.Controllers
 {
@@ -25,28 +26,37 @@ namespace UdemyBook.Areas.Admin.Controllers
 
         public IActionResult Create()
         {
-            IEnumerable<SelectListItem> CategoryList = _unitOfWork.Category
-                .GetAll().Select(u => new SelectListItem
+            ProductVM productVM = new()
+            {
+                CategoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem
                 {
                     Text = u.Name,
                     Value = u.Id.ToString()
-                });
-
-            ViewBag.CategoryList = CategoryList;
-
-            return View();
+                }),
+            Product = new Product()
+            };
+            return View(productVM);
         }
         [HttpPost]
-        public IActionResult Create(Product obj)
+        public IActionResult Create(ProductVM productVM)
         {
             if (ModelState.IsValid)
             {
-                _unitOfWork.Product.Add(obj);
+                _unitOfWork.Product.Add(productVM.Product);
                 _unitOfWork.Save();
                 TempData["success"] = "Product created successfully";
                 return RedirectToAction("Index");
             }
-            return View();
+            else
+            {
+                productVM.CategoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+
+                });
+                return View(productVM);
+            }
         }
         public IActionResult Edit(int? id)
         {
