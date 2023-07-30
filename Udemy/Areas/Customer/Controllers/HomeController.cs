26251e4e4f82310b80901e739fd5c05db7fcc3e8
Alexey Krasnokutskiy <allxyog@gmail.com>
2023-07-30 05:10:44 +0200

    Done

--- Udemy/Areas/Customer/Controllers/HomeController.cs
+++ Udemy/Areas/Customer/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using UdemyBook.DataAccess.Repository.IRepository;
 using UdemyBook.Models;
 
 namespace UdemyBook.Areas.Customer.Controllers
@@ -8,15 +9,24 @@ namespace UdemyBook.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category");
+            return View(productList);
+        }
+
+        public IActionResult Details(int productId)
+        {
+            Product product = _unitOfWork.Product.Get(u=>u.Id== productId, includeProperties: "Category");
+            return View(product);
         }
 
         public IActionResult Privacy()
