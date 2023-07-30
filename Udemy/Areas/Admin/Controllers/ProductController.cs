c89d7c2291d1fd41531e0cada45d8f4d02d5454f
Alexey Krasnokutskiy <allxyog@gmail.com>
2023-07-30 02:38:50 +0200

    Absicherung

--- Udemy/Areas/Admin/Controllers/ProductController.cs
+++ Udemy/Areas/Admin/Controllers/ProductController.cs
@@ -159,5 +159,16 @@ namespace UdemyBook.Areas.Admin.Controllers
             TempData["success"] = "Product deleted successfully";
             return RedirectToAction("Index");
         }
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<Product> objProductList = _unitOfWork.Product.GetAll(includeProperties: "Category").ToList();
+            return Json(new { data = objProductList });
+        }
+
+        #endregion
     }
 }
---
+++ Udemy/wwwroot/js/product.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": { url:'/admin/product/getall'},
+        "columns": [
+            { data: 'title', "width": "25%" },
+            { data: 'isbn', "width": "15%" },
+            { data: 'ListPrice', "width": "10%" },
+            { data: 'author', "width": "20%" },
+            { data: 'category.name', "width": "15%" }
+        ]
+    });
+}
