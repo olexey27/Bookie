b56a7e5d6aa05a6e419f8c0dd0efda946513d9ef
Alexey Krasnokutskiy <allxyog@gmail.com>
2023-08-06 01:31:40 +0200

    Login

--- Udemy.DataAccess/Data/ApplicationDbContext.cs
+++ Udemy.DataAccess/Data/ApplicationDbContext.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using UdemyBook.Models;
 
 namespace UdemyBook.DataAcess.Data
 {
-    public class ApplicationDbContext : DbContext
-    {
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+	{
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
 
@@ -15,6 +17,8 @@ namespace UdemyBook.DataAcess.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
+
             modelBuilder.Entity<Category>().HasData(
                 new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
                 new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
--- Udemy/Program.cs
+++ Udemy/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using UdemyBook.DataAccess.Repository;
 using UdemyBook.DataAccess.Repository.IRepository;
 using UdemyBook.DataAcess.Data;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options=>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
